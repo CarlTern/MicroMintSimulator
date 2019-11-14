3aea75466fbe043cae20583ef5a400cba76cc2e9
Diocles1 <vi1221pe-s@student.lu.se>
2019-11-14 21:38:52 +0100

    Simulation is now working

--- MicroMintSimulator.py
+++ MicroMintSimulator.py
@@ -3,6 +3,7 @@ import hashlib
 import random
 import statistics
 import math
+import time
 
 def getParameters():
     try:
@@ -12,68 +13,65 @@ def getParameters():
         widthOfConfidenceInterval = sys.argv[4]
         if (u is False or k is False or c is False or widthOfConfidenceInterval is False):
             print("Invalid parameters")
+            print("Usage:", '"MicroMintSimulator u k c w"')
+            print("For example:", '"python3 MicroMintSimulator.py 16 2 10000 22"')
             exit()
         else:
             return {"u":u, "k":k, "c":c, "woci": widthOfConfidenceInterval}
     except:
         print("Invalid parameters")
+        print("Usage:", '"MicroMintSimulator u k c w"')
+        print("For example:", '"python3 MicroMintSimulator.py 16 2 10000 22"')
         exit()
 
 def throwBalls(k, u, c):
-    bins = {}
+    bins = {} # Dictonary, compare to Map. 
     mintedCoins = 0
     tossedBalls = 0
     usedBins = []
-    numberOfTossesNeededForMinting = []
     while(mintedCoins < c):
         tossedBalls += 1
-        binIndex = random.randrange(2**u)
-        #binHash = hashlib.md5(binIndex.to_bytes(100, byteorder='big')).hexdigest()
-        binKey = "bin" + str(binIndex)
-        if(binKey in bins):
-            bins[binKey] += 1
+        binIndex = str(random.randrange(2**u))
+        if(binIndex in bins):
+            bins[binIndex] += 1 # If it exist we increment. 
         else:
-            bins[binKey] = 1
+            bins[binIndex] = 1 # Otherwise add it.
         
-        if(bins[binKey] == k and binKey not in usedBins):
+        if(bins[binIndex] == k and binIndex not in usedBins): # Bingo! we have a match and it is not used before
             mintedCoins +=1
-            numberOfTossesNeededForMinting.append(tossedBalls)
-            tossedBalls = 0
-            usedBins.append(binKey)
-    #print(mintedCoins, "coins has been minted!")
-    #print("Threw", tossedBalls, "balls in total")
-    return numberOfTossesNeededForMinting
+            usedBins.append(binIndex)
+
+    return tossedBalls
 
 def calculateConfidenceIntervalDifference(data, simulationCounter, mean, deviation, lamda):
     minValue = mean - (lambdaValue * (deviation/math.sqrt(simulationCounter)))
     maxValue = mean + (lambdaValue * (deviation/math.sqrt(simulationCounter)))
+
     return maxValue - minValue
     
-
 if __name__ == "__main__":
-    #parameters = getParameters()
-    #u = int(parameters["u"])
-    #k = int(parameters["k"])
-    #c = int(parameters["c"])
-    #widthOfConfidenceInterval = int(parameters["woci"])
-    u = 16
-    k = 2
-    c = 1
-    widthOfConfidenceInterval = 22
-    #bins = createBins(u)
+    parameters = getParameters()
+    u = int(parameters["u"])
+    k = int(parameters["k"])
+    c = int(parameters["c"])
+    widthOfConfidenceInterval = int(parameters["woci"])
     data = []
     simulationCounter = 0
     lambdaValue = 3.66
     calculatedWidth = widthOfConfidenceInterval + 1 # This way we will allways enter the while loop.
+    print("Simulation starts!")
+    start = time.time()
     while(calculatedWidth > widthOfConfidenceInterval):
         simulationCounter += 1
-        data += throwBalls(k, u, c)
-        if(len(data) < 2): # Without alleast 2 values we cant calculate deviation. 
+        data.append(throwBalls(k, u, c))
+        if(len(data) < 2): # Without alleast 2 values we can't calculate deviation. 
             continue
         mean = statistics.mean(data)
         deviation = statistics.stdev(data)
         calculatedWidth = calculateConfidenceIntervalDifference(data, simulationCounter, mean, deviation, lambdaValue)
-        print(calculatedWidth)
+        print("Width of confidence interval:", calculatedWidth)
+    end = time.time()
     print("The mean value is:", statistics.mean(data))
+    print("Time of simulation:", (end - start) / 60, "minutes")
 
 

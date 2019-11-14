ccc6f72daf9b7035558a9ba31168aab91239529a
VikPet <viktor.pettersson@debricked.com>
2019-11-14 16:53:01 +0100

    First version of simulation

--- MicroMintSimulator.py
+++ MicroMintSimulator.py
@@ -2,6 +2,7 @@ import sys
 import hashlib
 import random
 import statistics
+import math
 
 def getParameters():
     try:
@@ -32,39 +33,43 @@ def throwBalls(bins, k, u, c):
     usedBins = []
     while(mintedCoins < c):
         tossedBalls += 1
+        print(tossedBalls)
         binIndex = random.randrange(2**int(u))
-        binHash = hashlib.md5(binIndex.to_bytes(4, byteorder='big')).hexdigest()
-        bins[binHash] += 1
-        if(bins[binHash] == k and binHash not in usedBins):
+        #binHash = hashlib.md5(binIndex.to_bytes(100, byteorder='big')).hexdigest()
+        bins[str(binIndex)] += 1
+        if(bins[binIndex] == k and str(binIndex) not in usedBins):
             mintedCoins +=1
-            usedBins.append(binHash)
+            usedBins.append(str(binIndex))
     print(mintedCoins, "coins has been minted!")
-    print("Threw", tossedBalls, "balls in total")
+    #print("Threw", tossedBalls, "balls in total")
     return tossedBalls
 
-def calculateConfidenceInterval(data, simulationCounter, mean, deviation):
+def calculateConfidenceIntervalDifference(data, simulationCounter, mean, deviation, lamda):
+    minValue = mean - (lambdaValue * (deviation/math.sqrt(simulationCounter)))
+    maxValue = mean + (lambdaValue * (deviation/math.sqrt(simulationCounter)))
+    return maxValue - minValue
     
 
 if __name__ == "__main__":
-    #parameters = getParameters()
-    #u = parameters["u"]
-    #k = parameters["k"]
-    #c = parameters["c"]
-    #widthOfConfidenceInterval = parameters["woci"]
-    u = 16
-    k = 2
-    c = 1
-    widthOfConfidenceInterval = 3
-    bins = createBins(u)
+    parameters = getParameters()
+    u = int(parameters["u"])
+    k = int(parameters["k"])
+    c = int(parameters["c"])
+    widthOfConfidenceInterval = int(parameters["woci"])
+    #bins = createBins(u)
     data = []
     simulationCounter = 0
     lambdaValue = 3.66
-    calculatedWidth = widthOfConfidenceInterval + 1 # This way we sill allways enter the while loop.
+    calculatedWidth = widthOfConfidenceInterval + 1 # This way we will allways enter the while loop.
     while(calculatedWidth > widthOfConfidenceInterval):
         simulationCounter += 1
-        data.append(throwBalls(bins, k, u, c))
+        data.append(throwBalls([], k, u, c))
+        if(len(data) < 2): # Without alleast 2 values we cant calculate deviation. 
+            continue
         mean = statistics.mean(data)
         deviation = statistics.stdev(data)
-        confInterval = calculateConfidenceInterval(data, simulationCounter, mean, deviation)
+        calculatedWidth = calculateConfidenceIntervalDifference(data, simulationCounter, mean, deviation, lambdaValue)
+        print(calculatedWidth)
+    print(statistics.mean(data))
 
 
